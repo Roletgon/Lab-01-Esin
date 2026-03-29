c29fc53d34587e8dc307e8725968e71984185f3d
Esin Andrew <esinban@gmail.com>
2026-03-30 06:06:05 +0700

    Реализован вариант № 3

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+
+char[] arr = input.ToCharArray();
+Array.Reverse(arr);
+
+Console.WriteLine("Результат: " + new string(arr));
+Console.Read();
